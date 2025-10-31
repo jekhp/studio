51203ef34cdab1247163cebb6f354c03ac065401
HJOVER KJUIRO HUAMAN <yoverkjuiroity@gmail.com>
2025-10-31 17:35:23 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -5,12 +5,13 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
 // Fix for default icon issue with webpack
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
 });
 
 interface LeafletMapProps {
